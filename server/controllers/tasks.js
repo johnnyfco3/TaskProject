1be3cbad609875a0977f1a80fcf821d3c558e31d
johnnytejada <johnnyfran20002@gmail.com>
2022-03-25 15:56:43 -0400

    api post methods for users/tasks/categories

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -2,9 +2,19 @@ const express = require("express")
 const app = express.Router();
 const taskModel = require("../models/task")
 
+const CREATED_STATUS = 201
+
 app
     .get('/', (req,res) =>{
         res.send(taskModel.tList)
+    })
+    .get('/:id', (req,res) =>{
+        const task = taskModel.get(req.params.id)
+        res.send(task)
+    })
+    .post('/', (req,res) =>{
+        const task = taskModel.create(req.body)
+        res.status(CREATED_STATUS).send(task)
     })
 
 module.exports = app
