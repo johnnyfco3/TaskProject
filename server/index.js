939512e656335bbbc05e820f37510021d12243cf
johnnytejada <johnnyfran20002@gmail.com>
2022-04-04 19:55:48 -0400

    creating token and authorizations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require('express')
 
+const userModel = require('./models/user')
 const usersController = require("./controllers/users")
 const tasksController = require("./controllers/tasks")
 const categoriesController = require("./controllers/categories")
+
+const { requireAuth } = require("./models/auth")
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,13 +15,33 @@ app
   .use('/', express.static(__dirname + '/public/'))
 
   .use(express.json())
+  .use((req, res, next) => {
+    const auth = req.headers.authorization
+    if (auth) {
+      const token = auth.split(' ')[1]
+      userModel.fromToken(token)
+        .then(user => {
+          req.user = user
+          next()
+          }).catch(next)
+    } 
+    else {
+      next()
+    }
+  })
 
   .get('/', (req, res) => {
     res.send('You are on the homepage')
   })
   .use('/api/users', usersController)
-  .use('/api/tasks', tasksController)
-  .use('/api/categories', categoriesController)
+  .use('/api/tasks', requireAuth, tasksController)
+  .use('/api/categories', requireAuth, categoriesController)
+
+  .use((err, req, res, next) => {
+    console.error(err)
+    res .status(err.statusCode || 500)
+        .send({errors: [ err.message ?? 'Internal server error' ]})
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
